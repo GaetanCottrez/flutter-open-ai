766ee9a2582ae9ed8263b42af603262351ccabeb
Gaëtan Cottrez <gaetan@passiverecords.com>
2023-03-12 21:48:03 +0100

    :fire: remove example api_response + changed api key

--- lib/core/open_ai_data.dart
+++ lib/core/open_ai_data.dart
@@ -1,4 +1,4 @@
-const String OPEN_AI_KEY = "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";
-
+const String OPEN_AI_KEY =
+    "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";
 
 const String baseURL = "https://api.openai.com/v1";
